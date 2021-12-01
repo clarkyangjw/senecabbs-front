d982964bf80c9861b5aae5f375462a72edf7c958
Clark <k345974523@gmail.com>
2021-12-01 16:53:43 -0500

    setup webapp structure

---
+++ src/pages/SubjectList/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SubjectList extends Component {
+    render() {
+        return (
+            <div>
+                SubjectList...
+            </div>
+        )
+    }
+}
